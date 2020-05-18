c3cf7d23451b8267c9dc642510e7979e86cfa8ba
dcamargo <diego.camargo-proveedor@credibanco.com>
2020-05-18 15:27:06 -0500

    Merge

--- test/lifegamecb/TestCell.java
+++ test/lifegamecb/TestCell.java
@@ -1,21 +1,41 @@
 package lifegamecb;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class TestCell {
+	
+	//Any live cell with fewer than two live neighbours dies, 
+	//as if by underpopulation.
 
-	// Any live cell with fewer than two live neighbours dies,
-	// as if by underpopulation.
 
 	@Test
 	void liveCelWithOnethanLiveNeighboursDies() {
-
-		Cell c = new Cell(true, 1);
+		
+		Cell c= new Cell(true,1 );
 		c.tick();
-		assertFalse(c.getStatus());// comentario prueba
-
+		assertFalse( c.getStatus());
+		
 	}
 
+	@Test
+	void liveCelWithZerothanLiveNeighboursDies() {
+		
+		Cell c= new Cell(true,0 );
+		c.tick();
+		assertFalse( c.getStatus());
+		
+	}
+	
+	//Any live cell with two or three live neighbours lives on to the next generation
+	
+	@Test
+	void liveCellWithTwoOrThreeLiveNeighboursLives () {
+		
+		Cell c= new Cell(true,2 );
+		c.tick();
+		assertTrue( c.getStatus());
+		
+	}
 }
