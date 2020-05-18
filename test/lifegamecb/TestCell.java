228f78e08287f1e25e63e2b47a330b5845815eb2
dcamargo <diego.camargo-proveedor@credibanco.com>
2020-05-18 15:21:53 -0500

    Prueba git

--- test/lifegamecb/TestCell.java
+++ test/lifegamecb/TestCell.java
@@ -1,22 +1,21 @@
 package lifegamecb;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
 class TestCell {
-	
-	//Any live cell with fewer than two live neighbours dies, 
-	//as if by underpopulation.
 
+	// Any live cell with fewer than two live neighbours dies,
+	// as if by underpopulation.
 
 	@Test
 	void liveCelWithOnethanLiveNeighboursDies() {
-		
-		Cell c= new Cell(true,1 );
+
+		Cell c = new Cell(true, 1);
 		c.tick();
-		assertFalse( c.getStatus());
-		
+		assertFalse(c.getStatus());// comentario prueba
+
 	}
 
 }
